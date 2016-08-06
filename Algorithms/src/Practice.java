2b5aece2e210af6776402aca394e9941bf8ac198
Harsh Mehta <hamehta3@gmail.com>
2016-08-06 16:00:07 -0400

    Add some path finding functions

--- Algorithms/src/Practice.java
+++ Algorithms/src/Practice.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Practice {
@@ -65,6 +66,112 @@ public class Practice {
 		return true;
 	}
 	
+	/*
+	 * http://s31.postimg.org/7nwp4gmzv/hurdle1.jpg
+	 * Basically DFS with backtracking
+	 * Assuming 0s for valid path, 1s for hurdles, 2 for destination
+	 */
+	public int longestDist(int [][] matrix, int x, int y) {
+		boolean [][] visited = new boolean[matrix.length][matrix[0].length];
+		return longestDist(matrix, 0, x, y, visited);
+	}
+	
+	/*
+	 * Runtime: O(4^n)
+	 */
+	private int longestDist(int [][] mat, int dist, int x, int y, boolean [][] visited) {
+		if (!isWithinBounds(mat, x, y)) {
+			return -1;
+		}
+
+		if (visited[x][y] ) {
+			return -1;
+		}
+
+		if (isHurdle(mat,x,y)) {
+			return -1;
+		}
+
+		if (isDest(mat,x,y)) {
+			return dist;
+		}
+
+		visited[x][y] = true;
+		int max = -1;
+		int d1 = longestDist(mat, dist+1, x+1, y, visited);
+		int d2 = longestDist(mat, dist+1, x, y+1, visited);
+		int d3 = longestDist(mat, dist+1, x, y-1, visited);
+		int d4 = longestDist(mat, dist+1, x-1, y, visited);
+		max = getMax(d1, d2, d3, d4);
+		visited[x][y] = false;
+		
+		return max;
+	}
+	
+	private boolean isDest(int [][] matrix, int x, int y) {
+		return matrix[x][y] == 2;
+	}
+	
+	private boolean isHurdle(int [][] matrix, int x, int y) {
+		return matrix[x][y] == 1;
+	}
+	
+	private int getMax(int a, int b, int c, int d) {
+		return Math.max(d, Math.max(Math.max(a, b), c));
+	}
+	
+	private boolean isWithinBounds(int [][] matrix, int x, int y) {
+		if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length) {
+			return false;
+		}
+		return true;
+	}
+	
+	/*
+	 * Find maximum number of paths from top left corner of matrix to bottom right
+	 * Can only move either right or down
+	 */
+	public int numPathsTopDown(int [][] matrix) {
+		return numPathsTopDown(matrix, 0, 0, new HashMap<String, Integer>());
+	}
+	
+	private int numPathsTopDown(int [][] mat, int x, int y, HashMap<String, Integer> map) {
+		if (!isWithinBounds(mat, x, y)) {
+			return 0;
+		}
+
+		if (map.containsKey(x+","+y)) {
+			return map.get(x+","+y);
+		}
+		
+		if (x == mat.length-1 && y == mat[0].length-1) {
+			return 1;
+		}
+		
+		int pathsFromHere = 0;
+		int p1 = numPathsTopDown(mat, x+1, y, map);
+		pathsFromHere += p1;
+		int p2 = numPathsTopDown(mat, x, y+1, map);
+		pathsFromHere += p2;
+		map.put(x+","+y, pathsFromHere);
+		return pathsFromHere;
+	}
+	
+	public int numPathsBottomUp(int [][] mat) {
+		mat[mat.length-1][mat[0].length-1] = 1;
+		for (int i=mat.length-1; i>=0; i--) {
+			for (int j=mat[0].length-1; j>=0; j--) {
+				if (i == mat.length-1 && j == mat[0].length-1) {
+					mat[i][j] = 1;
+				} else {
+					mat[i][j] = (j+1 >= mat[0].length ? 0 : mat[i][j+1])
+							+ (i+1 >= mat.length ? 0 : mat[i+1][j]);
+				}
+			}
+		}
+		return mat[0][0];
+	}
+	
 	public static void main(String args[]) {
 		Practice p = new Practice();
 		System.out.println(p.parseIp("103721"));
@@ -73,5 +180,16 @@ public class Practice {
 		System.out.println(p.parseIp("3456"));
 		System.out.println(p.parseIp("000000"));
 		System.out.println(p.parseIp("847"));
+		
+		int [][] matrix = new int [3][10];
+		matrix[1][2] = 1;
+		matrix[1][5] = 1;
+		matrix[1][8] = 1;
+		matrix[1][7] = 2;
+		System.out.println(p.longestDist(matrix, 0, 0));
+		
+		int [][] mat = new int[4][4];
+		System.out.println(p.numPathsTopDown(mat));
+		System.out.println(p.numPathsBottomUp(mat));
 	}
 }
